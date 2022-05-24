ab8b56ff8a8ce8ee024cc0af3499f82e0345b466
leonard33 <leonardkombo14@gmail.com>
2022-05-24 16:19:16 +0300

    Add calculator design

---
+++ src/components/Calculator.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './Calculate.css'
+
+const createDigit = () => {
+  let digits = [];
+  for (let i=9; i>=1; i--){
+     digits.push(<button key={i}>{i}</button>)
+  }
+  return digits;
+};
+
+export default class Calculator extends Component {
+  render() {
+    return (
+      <div className='calculate'>
+        <div className='display'><span>0</span></div>
+        <div className='calc-digits'>
+        <div className='calc-left'>
+        <div className='operations-top'>
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+        </div>
+        <div className='digits'>
+          {createDigit()}
+          <button>0</button>
+          <button>.</button>
+        </div>
+        </div>
+        <div className='calc-right'>
+        <div className='operations-right'>
+          <button>/</button>
+          <button>*</button>
+          <button>-</button>
+          <button>=</button>
+        </div>
+        </div>
+        </div>
+      </div>
+    )
+  }
+}
