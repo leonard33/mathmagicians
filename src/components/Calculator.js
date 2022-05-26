c4efef73b0d762a18b4052c2ccaaabb2bda78ce2
leonard33 <leonardkombo14@gmail.com>
2022-05-26 17:37:44 +0300

    pr using functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,53 @@
-import React, { Component } from 'react';
+import {React, useState} from 'react'
 import './Calculate.css';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // calc: "",
-      total: '',
-      next: '',
-      operations: '',
-    };
-    this.updateState = this.updateState.bind(this);
-  }
+export default function Calculator() {
+const [calc, setCalc] = useState({ total: null, next: null, operations: null });
 
   // state updating handler
-  updateState = (buttonName) => {
-    const temp = { ...this.state };
-    this.setState(calculate(temp, buttonName));
+ const updateState = (buttonName) => {
+   let temp = {};
+     temp = { ...calc };
+    setCalc(calculate(temp, buttonName));
   }
 
-  // creating the digits 1-9
-  createDigit = () => {
-    const digits = [];
-    for (let i = 9; i >= 1; i -= 1) {
-      // eslint-disable-next-line no-loop-func
-      digits.push(<button type="button" onClick={() => this.updateState(i.toString())} key={i}>{i}</button>);
-    }
-    return digits;
-  };
-
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="calculate">
-        <div className="display"><span>{next || total || '0'}</span></div>
+ // creating the digits 1-9
+const createDigit = () => {
+  const digits = [];
+  for (let i = 9; i >= 1; i -= 1) {
+    // eslint-disable-next-line no-loop-func
+    digits.push(<button type="button" onClick={() => updateState(i.toString())} key={i}>{i}</button>);
+  }
+  return digits;
+};
+ // console.log(calc)
+  return (
+    <div className="calculate">
+        <div className="display"><span>{calc.next || calc.total || '0'}</span></div>
         <div className="calc-digits">
           <div className="calc-left">
             <div className="operations-top">
-              <button type="button" onClick={() => this.updateState('AC')}>AC</button>
-              <button type="button" onClick={() => this.updateState('+/-')}>+/-</button>
-              <button type="button" onClick={() => this.updateState('%')}>%</button>
+              <button type="button" onClick={() => updateState('AC')}>AC</button>
+              <button type="button" onClick={() => updateState('+/-')}>+/-</button>
+              <button type="button" onClick={() => updateState('%')}>%</button>
             </div>
             <div className="digits">
-              {this.createDigit()}
-              <button type="button" onClick={() => this.updateState('0')}>0</button>
-              <button type="button" onClick={() => this.updateState('.')}>.</button>
+              {createDigit()}
+              <button type="button" onClick={() => updateState('0')}>0</button>
+              <button type="button" onClick={() => updateState('.')}>.</button>
             </div>
           </div>
           <div className="calc-right">
             <div className="operations-right">
-              <button type="button" onClick={() => this.updateState('÷')}>/</button>
-              <button type="button" onClick={() => this.updateState('x')}>*</button>
-              <button type="button" onClick={() => this.updateState('+')}>+</button>
-              <button type="button" onClick={() => this.updateState('-')}>-</button>
-              <button type="button" onClick={() => this.updateState('=')}>=</button>
+              <button type="button" onClick={() => updateState('÷')}>/</button>
+              <button type="button" onClick={() => updateState('x')}>*</button>
+              <button type="button" onClick={() => updateState('+')}>+</button>
+              <button type="button" onClick={() => updateState('-')}>-</button>
+              <button type="button" onClick={() => updateState('=')}>=</button>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+  )
 }
