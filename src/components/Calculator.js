ee3ca6530025cd174f440871f38f6129d90ec96d
leonard33 <leonardkombo14@gmail.com>
2022-05-26 17:46:20 +0300

    fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,31 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react';
 import './Calculate.css';
 import calculate from '../logic/calculate';
 
 export default function Calculator() {
-const [calc, setCalc] = useState({ total: null, next: null, operations: null });
+  const [calc, setCalc] = useState({ total: null, next: null, operations: null });
 
   // state updating handler
- const updateState = (buttonName) => {
-   let temp = {};
-     temp = { ...calc };
+  const updateState = (buttonName) => {
+    let temp = {};
+    temp = { ...calc };
     setCalc(calculate(temp, buttonName));
-  }
+  };
 
- // creating the digits 1-9
-const createDigit = () => {
-  const digits = [];
-  for (let i = 9; i >= 1; i -= 1) {
+  // creating the digits 1-9
+  const createDigit = () => {
+    const digits = [];
+    for (let i = 9; i >= 1; i -= 1) {
     // eslint-disable-next-line no-loop-func
-    digits.push(<button type="button" onClick={() => updateState(i.toString())} key={i}>{i}</button>);
-  }
-  return digits;
-};
- // console.log(calc)
+      digits.push(<button type="button" onClick={() => updateState(i.toString())} key={i}>{i}</button>);
+    }
+    return digits;
+  };
+
   return (
     <div className="calculate">
-        <div className="display"><span>{calc.next || calc.total || '0'}</span></div>
-        <div className="calc-digits">
+      <div className="display"><span>{calc.next || calc.total || '0'}</span></div>
+      <div className="calc-digits">
           <div className="calc-left">
             <div className="operations-top">
               <button type="button" onClick={() => updateState('AC')}>AC</button>
@@ -48,6 +48,6 @@ const createDigit = () => {
             </div>
           </div>
         </div>
-      </div>
-  )
+    </div>
+  );
 }
