9aad5c8af1879afd4ef1884368a46cab6604ddb0
leonard33 <leonardkombo14@gmail.com>
2022-05-26 12:55:16 +0300

    logic included

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,44 +1,72 @@
 import React, { Component } from 'react';
 import './Calculate.css';
+import  calculate from '../logic/calculate.js';
 
-const createDigit = () => {
-  const digits = [];
-  for (let i = 9; i >= 1; i -= 1) {
-    digits.push(<button type="button" key={i}>{i}</button>);
-  }
-  return digits;
-};
 
+// const operation = ['-', '+', '/', '*', '.'];
+// console.log(operation);
 export default class Calculator extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      // calc: "",
+      total: "",
+      next: "",
+      operations: ""
+    };
+    this.updateState = this.updateState.bind(this)
+    
+  }
+  /*
+  updateState = (value) => { 
+    // Changing state 
+    /* if((operation.includes(value) && this.calc ==='') || (operation.includes(value) && operation.includes(this.calc.slice(-1)))){
+      return;
+    } 
+    this.setState({calc: this.state.calc + value});
+    // this.calculate(this.calc, value);
+  } */
+
+// state updating handler
+  updateState = (buttonName) => {
+    const temp = {...this.state}
+    this.setState(calculate(temp, buttonName))
   }
 
+// creating the digits 1-9
+  createDigit = () => {
+    const digits = [];
+    for (let i = 9; i >= 1; i -= 1) {
+      // eslint-disable-next-line no-loop-func
+      digits.push(<button type="button" onClick={() => this.updateState(i.toString())} key={i}>{i}</button>);
+    }
+    return digits;
+  };
   render() {
     return (
       <div className="calculate">
-        <div className="display"><span>0</span></div>
+        <div className="display"><span>{this.state.next|| this.state.total || "0"}</span></div>
         <div className="calc-digits">
           <div className="calc-left">
             <div className="operations-top">
-              <button type="button">AC</button>
-              <button type="button">+/-</button>
-              <button type="button">%</button>
+              <button type="button" onClick={() => this.updateState('AC')}>AC</button>
+              <button type="button" onClick={() => this.updateState('+/-')}>+/-</button>
+              <button type="button" onClick={() => this.updateState('%')}>%</button>
             </div>
             <div className="digits">
-              {createDigit()}
-              <button type="button">0</button>
-              <button type="button">.</button>
+              {this.createDigit()}
+              <button type="button" onClick={() => this.updateState('0')}>0</button>
+              <button type="button" onClick={() => this.updateState('.')}>.</button>
             </div>
           </div>
           <div className="calc-right">
             <div className="operations-right">
-              <button type="button">/</button>
-              <button type="button">*</button>
-              <button type="button">+</button>
-              <button type="button">-</button>
-              <button type="button">=</button>
+              <button type="button" onClick={() => this.updateState('÷')}>/</button>
+              <button type="button" onClick={() => this.updateState('x')}>*</button>
+              <button type="button" onClick={() => this.updateState('+')}>+</button>
+              <button type="button" onClick={() => this.updateState('-')}>-</button>
+              <button type="button" onClick={() => this.updateState('=')}>=</button>
             </div>
           </div>
         </div>
