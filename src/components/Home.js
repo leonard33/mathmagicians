b2ae93c107405da04ce404437b33fd052348cc0b
leonard33 <leonardkombo14@gmail.com>
2022-06-01 18:45:18 +0300

    fix linters

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Home.css';
 
 export default function Home() {
-     return (
+  return (
        <>
-      <div>
-          <h2>Welcome to our page !</h2>
+         <div>
+        <h2>Welcome to our page !</h2>
       </div>
-         <div className="loren">
+          <div className="loren">
           Enjoy using the calculator to solve your mathematical problems
-      </div>
+         </div>
        </>
   );
 }
